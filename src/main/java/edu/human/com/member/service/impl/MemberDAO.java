9b2d26e22f6be759759ddfc6f0bcf701fe3c0522
User <User@DESKTOP-6223J9B>
2021-02-15 10:51:23 +0900

    2교시 커밋 입니다.

--- src/main/java/edu/human/com/member/service/impl/MemberDAO.java
+++ src/main/java/edu/human/com/member/service/impl/MemberDAO.java
@@ -17,7 +17,23 @@ import edu.human.com.member.service.EmployerInfoVO;
 public class MemberDAO extends EgovComAbstractMapper {
 	
 	public List<EmployerInfoVO> selectMember() throws Exception {
-		
 		return selectList("memberMapper.selectMember");
+	}
+	
+	public EmployerInfoVO viewMember(String emplyr_id) throws Exception {
+		//selctOne("쿼리ID","쿼리매개변수=파라미터=인자");
+		return selectOne("memberMapper.viewMember", emplyr_id);
+	}
+	
+	public void deleteMember(String emplyr_id) throws Exception {
+		delete("memberMapper.deleteMember", emplyr_id);
+	}
+	
+	public void insertMember(EmployerInfoVO employerInfoVO) throws Exception {
+		insert("memberMapper.insertMember", employerInfoVO);
+	}
+	
+	public void updateMember(EmployerInfoVO employerInfoVO) throws Exception {
+		update("memberMapper.updateMember", employerInfoVO);
 	}
 }
