3ec74069f7c0d1e6007081c90ec9f2311f11e912
User <User@DESKTOP-6223J9B>
2021-02-18 15:06:08 +0900

    중간 커밋

--- src/main/java/edu/human/com/admin/web/AdminController.java
+++ src/main/java/edu/human/com/admin/web/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.human.com.member.service.EmployerInfoVO;
 import edu.human.com.member.service.MemberService;
+import egovframework.let.utl.sim.service.EgovFileScrty;
 
 @Controller
 public class AdminController {
@@ -50,8 +51,11 @@ public class AdminController {
 	}
 	@RequestMapping(value="/admin/member/insert_member.do",method=RequestMethod.POST)
 	public String insert_member(EmployerInfoVO memberVO,RedirectAttributes rdat) throws Exception {
-		//입력DB처리 호출
-		
+		//입력DB처리 호출: 1.암호를 egov암호화툴로 암호, 2.ESNTL_ID 고유ID(게시판관리자ID) 생성
+		String formPassword = memberVO.getPASSWORD();//jsp입력폼에서 전송된 암호값GET
+		String encPassword = EgovFileScrty.encryptPassword(formPassword, memberVO.getESNTL_ID());
+		memberVO.setPASSWORD(encPassword);//egov암호화툴로 암호화된 값SET
+		memberVO.setESNTL_ID("USRCNFRM_" + memberVO.getESNTL_ID());//고유ID값 SET
 		memberService.insertMember(memberVO);
 		rdat.addFlashAttribute("msg", "입력");
 		return "redirect:/admin/member/list_member.do";
@@ -59,6 +63,11 @@ public class AdminController {
 	@RequestMapping(value="/admin/member/update_member.do",method=RequestMethod.POST)
 	public String update_member(EmployerInfoVO memberVO,RedirectAttributes rdat) throws Exception {
 		//회원 수정 페이지 DB처리
+		if(memberVO.getPASSWORD() != null) {
+			String formPassword = memberVO.getPASSWORD();//GET
+			String encPassword = EgovFileScrty.encryptPassword(formPassword, memberVO.getESNTL_ID());
+			memberVO.setPASSWORD(encPassword);//SET
+		}
 		memberService.updateMember(memberVO);
 		rdat.addFlashAttribute("msg", "수정");//아래 view_member.jsp로 변수 msg값을 전송합니다.
 		return "redirect:/admin/member/view_member.do?emplyr_id=" + memberVO.getEMPLYR_ID();
