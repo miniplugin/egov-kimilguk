38b3d82215ef7703fb86ff551f4cbadd9f1ffc50
boramcom@daum.net <boramcom@daum.net>
2021-03-15 17:47:41 +0900

    8교시 커밋 및 내일 수업 칠판정리

--- src/main/java/edu/human/com/home/web/HomeController.java
+++ src/main/java/edu/human/com/home/web/HomeController.java
@@ -538,8 +538,9 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/logout.do")
-	public String logout() throws Exception {
+	public String logout(HttpServletRequest request) throws Exception {
 		RequestContextHolder.getRequestAttributes().removeAttribute("LoginVO", RequestAttributes.SCOPE_SESSION);
+		request.getSession().invalidate();//LoginVO세션값이 현재 URL의 모든세션 날림.
 		return "redirect:/";
 	}
 	//method.RequestMethod=GET[POST] 없이사용하면, 둘다 허용되는 매핑이됨
